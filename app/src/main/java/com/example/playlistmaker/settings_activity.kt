064ef3d06932b0479fa9f9bbf71a9a8ced670647
AleksandrPolistsuk <luckycampshop@gmail.com>
2025-02-05 11:41:11 +0200

    commit 3

--- app/src/main/java/com/example/playlistmaker/settings_activity.kt
+++ app/src/main/java/com/example/playlistmaker/settings_activity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
@@ -9,10 +10,17 @@ import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textview.MaterialTextView
 
 class settings_activity : AppCompatActivity() {
+
+    private lateinit var themeSwitch: SwitchMaterial
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,22 +28,19 @@ class settings_activity : AppCompatActivity() {
         val back = findViewById<LinearLayout>(R.id.back)
 
         back.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-
-        finish()
+                 finish()
         }
-        val shareImageView: ImageView = findViewById(R.id.share)
+        val shareImageView: MaterialTextView = findViewById(R.id.share)
 
         shareImageView.setOnClickListener {
             shareApp()
         }
-        val contactSupportButton: ImageView = findViewById(R.id.support)
+        val contactSupportButton: MaterialTextView = findViewById(R.id.support)
 
         contactSupportButton.setOnClickListener {
             sendSupportEmail()
         }
-        val userAgreementButton: ImageView = findViewById(R.id.agreements)
+        val userAgreementButton: MaterialTextView = findViewById(R.id.agreements)
         userAgreementButton.setOnClickListener {
             openUserAgreement()
         }
@@ -45,18 +50,43 @@ class settings_activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        // Инициализация SharedPreferences для сохранения состояния переключателя
+        sharedPreferences = getSharedPreferences("settings", MODE_PRIVATE)
+        val isNightMode = sharedPreferences.getBoolean("night_mode", false)
+
+        // Найдем наш SwitchMaterial
+        themeSwitch = findViewById(R.id.switcher)
+
+        // Устанавливаем текущее состояние Switch (чтобы при перезапуске он не сбрасывался)
+        themeSwitch.isChecked = isNightMode
+
+        // Устанавливаем текущую тему
+        if (isNightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        // Обрабатываем нажатие на Switch
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPreferences.edit().putBoolean("night_mode", true).apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPreferences.edit().putBoolean("night_mode", false).apply()
+            }
+        }
     }
 
     private fun openUserAgreement() {
-        val url = getString(R.string.url)
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-
-        if (intent.resolveActivity(packageManager) != null) {
+            val url = getString(R.string.url)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
-        } else {
-            showToast(getString(R.string.browsError))
-        }
     }
+
     private fun sendSupportEmail() {
         val email = getString(R.string.eMail)
         val subject = getString(R.string.subject)
@@ -68,13 +98,8 @@ class settings_activity : AppCompatActivity() {
             putExtra(Intent.EXTRA_SUBJECT, subject)
             putExtra(Intent.EXTRA_TEXT, body)
         }
+      startActivity(intent)
 
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-
-        } else {
-            showToast(getString(R.string.noMail))
-        }
     }
 
     private fun showToast(message: String) {
